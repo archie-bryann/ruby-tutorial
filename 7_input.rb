332a5e090810e9153857ffc65d48563b9a690ef7
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-09-05 15:32:19 +0100

    input done

--- 7_input.rb
+++ 7_input.rb
@@ -1,2 +1,6 @@
 puts "Enter Your Name: "
-gets
+# name = gets # new line on "Enter"
+name = gets.chomp() # gets rid of new line character on "Enter"
+puts "Enter Your Age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age)
