8fa807f4540a5a3c0994e36c9964dbd103800f9b
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-09-05 14:55:13 +0100

    math and numbers

---
+++ 6_math_numbers.rb
@@ -0,0 +1,24 @@
+puts 5
+puts 5.8
+puts -10
+puts 5 * 9
+puts 10 / 4
+puts 2 ** 3
+puts 10 % 3
+num = 20
+puts ("my fav num " + num.to_s)
+num2 = -20
+puts num2.abs()
+nums3 = 20.687
+puts nums3.round()
+puts nums3.ceil()
+puts nums3.floor()
+
+puts Math.sqrt(36)
+puts Math.log(1)
+
+puts 1 + 7 # get an integer back
+puts 1.0 + 7 # get a floating point back
+
+puts 10 / 7 # get an integer back
+puts 10.0 / 7.0 # get a floating point back
