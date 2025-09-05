53ba90cb77e6d894a9021582bf52aba01f607f77
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-09-05 14:23:00 +0100

    strings

---
+++ 5_strings.rb
@@ -0,0 +1,19 @@
+puts "Giraffe\" Academy"
+puts "Giraffe\nAcademy" # new line
+
+phrase = "Giraffe Academy"
+puts phrase
+puts phrase.upcase() # upper case
+puts phrase.downcase() # lower case
+puts phrase.length()
+puts phrase.include? "Academy" # true
+puts phrase.include? "Academys" # false
+puts phrase[0] # first character
+puts phrase[0,3] # first three characters (index, range)
+puts phrase.index("G")
+puts phrase.index("ffe")
+
+phrase2 = "   Giraffe Academy   "
+puts phrase.strip()
+
+puts "programming".upcase()
